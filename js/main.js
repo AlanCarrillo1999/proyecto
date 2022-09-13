458b6ff70a26c04903e1dbfebea3f99cba8c9d79
AlanCarrillo1999 <113477279+AlanCarrillo1999@users.noreply.github.com>
2022-09-13 17:51:28 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -26,10 +26,10 @@ Author: GrayGrids
         var logo = document.querySelector('.navbar-brand img')
         if (window.pageYOffset > sticky) {
           header_navbar.classList.add("sticky");
-          logo.src = 'assets/images/logo/pink-ink.jpg';
+          logo.src = 'images/logo/pink-ink.jpg';
         } else {
           header_navbar.classList.remove("sticky");
-          logo.src = 'assets/images/logo/pink-ink.jpg';
+          logo.src = 'images/logo/pink-ink.jpg';
         }
 
         // show or hide the back-top-top button
